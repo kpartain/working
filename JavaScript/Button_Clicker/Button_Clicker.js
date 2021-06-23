eed1a618755c4c96431d8162af06e8bef4860cfe
kpartain <karalynnleann@gmail.com>
2021-06-23 14:58:18 -0700

    Starts Dictionary Assignment

---
+++ JavaScript/Button_Clicker/Button_Clicker.js
@@ -0,0 +1,16 @@
+//login/logout
+function loginLogout(){
+    var btn = document.getElementById("loginLogout");
+    if(btn.innerText == "login") {
+        btn.innerText = "logout";
+    } else {
+        btn.innerText = "login";
+    }
+}
+//add a word
+
+//
+function increaseLikes(){
+
+}
+
