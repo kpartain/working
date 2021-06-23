845defa9a17db38ffc600b55984fc37ad996d900
kpartain <karalynnleann@gmail.com>
2021-06-23 16:15:46 -0700

    Adds increaseLikes() functionality

--- JavaScript/Button_Clicker/Button_Clicker.js
+++ JavaScript/Button_Clicker/Button_Clicker.js
@@ -1,7 +1,7 @@
 //login/logout
-function loginLogout(){
+function loginLogout() {
     var btn = document.getElementById("loginLogout");
-    if(btn.innerText == "login") {
+    if (btn.innerText == "login") {
         btn.innerText = "logout";
     } else {
         btn.innerText = "login";
@@ -9,8 +9,14 @@ function loginLogout(){
 }
 //add a word
 
-//
-function increaseLikes(){
-
+//increase likes - look at THIS keyword?
+function increaseLikes(element) {
+    //grab current inner text of THIS
+    var entireText = element.innerText;
+    //split the string to isolate the string of the number
+    var split = entireText.split(' ');
+    //convert the string into an int, increment it by one
+    var likeCount = parseInt(split[0]) + 1;
+    //return the element text to reflect increased value
+    return element.innerText = likeCount + " Likes"
 }
-
