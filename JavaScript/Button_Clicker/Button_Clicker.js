08862e91835df04082837977bbffe161f29fbc0a
kpartain <karalynnleann@gmail.com>
2021-06-23 17:24:03 -0700

    Updates button_clicker

--- JavaScript/Button_Clicker/Button_Clicker.js
+++ JavaScript/Button_Clicker/Button_Clicker.js
@@ -8,15 +8,20 @@ function loginLogout() {
     }
 }
 //add a word
-
+function addDefinition(element){
+    return element.style.display = "none";
+}
 //increase likes - look at THIS keyword?
 function increaseLikes(element) {
+    var parent = element.parentNode.id
     //grab current inner text of THIS
     var entireText = element.innerText;
     //split the string to isolate the string of the number
     var split = entireText.split(' ');
     //convert the string into an int, increment it by one
     var likeCount = parseInt(split[0]) + 1;
+    //alert was liked
+    alert(parent + " was liked")
     //return the element text to reflect increased value
     return element.innerText = likeCount + " Likes"
 }
