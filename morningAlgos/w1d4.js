1ed398210bc0f4b1b88d3b07e811c1ad487d8e10
kpartain <karalynnleann@gmail.com>
2021-06-24 09:43:39 -0700

    Completes w1d4

--- morningAlgos/w1d4.js
+++ morningAlgos/w1d4.js
@@ -10,18 +10,20 @@ setX(15);
 console.log(x);
 //T-Chart Example X
 //Variable | Value
+// x       | 5 ; 15
 //         |
 //         |
 //         |
-//         |
-// c.log() |
+// c.log() | 5 ; 15
+
+
 
 //The Return of return
 var x = 5;
 
 function addToX(amount) {
     return x + amount;
-    console.log("hello there");
+    console.log("hello there"); //this is never reached
 }
 
 console.log(x);
@@ -30,11 +32,11 @@ console.log(result);
 console.log(x);
 //T-Chart Example X
 //Variable | Value
-//         |
-//         |
-//         |
-//         |
-// c.log() |
+//  x      | 5 ; 
+//  result | -5 ; 
+// c.log() | 5 ; -5 ; 5
+
+
 
 //Code Flow - Is the Array a Palindrome
 function isPal(arr) {
@@ -52,10 +54,10 @@ console.log(result1);
 
 var result2 = isPal([3, 2, 1, 1, 2, 3]);
 console.log(result2);
-//T-Chart Example X
+//T-Chart 1
 //Variable | Value
-//         |
-//         |
-//         |
-//         |
-// c.log() |
+//  result1| "not a pal-indrome!"
+// f left  | 0 ; 1 ;; 0 ; 1 ; 2 ; 3
+// f right | 4 ; 3 ;; 5 ; 4 ; 3
+// result2 | "Pal-indrome!"
+// c.log() | "not a pal-indrome!" ; "Pal-indrome!"
