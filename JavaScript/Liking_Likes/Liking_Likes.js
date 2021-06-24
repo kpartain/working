d958ce1b69ca3fe37c8ce78d301464ce96a542d3
kpartain <karalynnleann@gmail.com>
2021-06-24 14:02:38 -0700

    Liking Likes Part 1

---
+++ JavaScript/Liking_Likes/Liking_Likes.js
@@ -0,0 +1,17 @@
+function increaseLikes(element) {
+    //change the count on the button and the prior element per assignment *button is extra
+    return [
+        (element.innerText = likeSplit(element.innerText)),
+        (document.querySelector("#likeCount").innerText = likeSplit(
+            document.querySelector("#likeCount").innerText
+        )),
+    ];
+}
+
+//helper function to remove redundancy of incrementing inner text likes
+function likeSplit(textFromElement) {
+    var split = textFromElement.split(" ");
+    var likeCount = parseInt(split[0]) + 1;
+    var returnTextForElement = likeCount + " Likes";
+    return returnTextForElement;
+}
