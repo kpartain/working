467649acf12b381b2c6f458a28449c18cb8fc2b7
kpartain <karalynnleann@gmail.com>
2021-06-29 15:09:08 -0700

    Continues attempt

--- JavaScript/4_Function_Calculator/script.js
+++ JavaScript/4_Function_Calculator/script.js
@@ -1,8 +1,5 @@
-//innerHTML of our display
-var displayContent = document.getElementById("display").innerHTML;
-
 let myEquation = [];
-let someNumber = "";
+let someNumber = '';
 
 //1st onclick, change innerHTML of display to number
 function press(element){
@@ -14,21 +11,39 @@ function press(element){
         document.getElementById("display").innerHTML = someNumber;
     } else if(element.className == "operator"){
             myEquation.push(someNumber);
+            someNumber = '';
             myEquation.push(element.innerHTML);
-            someNumber = "";
+            // someNumber += element.innerHTML;
+            
     }
 }
 
-function calculate(element) {
+
+function calculate() {
+    //add last number to the array 
     myEquation.push(someNumber);
-    var resultString = myEquation.join("");
-    return console.log(eval(resultString));
-    // someNumber = "";
-    // console.log(eval(resultString));
+    //reset the string someNumber to blank
+    someNumber = '';
+    console.log("myEquation in calculate(): " + myEquation);
+    //turn the equation array into a string
+    var resultString = myEquation.join(" ");
+    console.log("resultString in calculate(): " + resultString);
+    //we want to set the equation to empty
+    myEquation = [];
+    //but then push the finalResult
+    var theAnswer = eval(resultString);
+    myEquation.push(theAnswer)
+    return console.log("Return of calculate(): " + theAnswer);
+
 }
+
+
 
 function clr(){
     //reset our equation operator to empty
     myEquation = [];
+    //reset our someNumber to empty 
+    someNumber = '';
+    //return the display to default 0
     return document.getElementById("display").innerHTML = 0;
 }
