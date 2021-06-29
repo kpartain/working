4badf8c3ce4acc9c967d1b2f76a5a20d3c238911
kpartain <karalynnleann@gmail.com>
2021-06-29 13:54:15 -0700

    Finishes Clock assignment

--- JavaScript/Ticking_Clock/script.js
+++ JavaScript/Ticking_Clock/script.js
@@ -1,11 +1,25 @@
 function getSecondsSinceStartOfDay() {
-    return new Date().getSeconds() + 
-      new Date().getMinutes() * 60 + 
-      new Date().getHours() * 3600;
-  }
+    var date = new Date();
+
+    var secondsOfDay = date.getSeconds();
+    var degreesSeconds = secondsOfDay * 6 - 180;
+    console.log(secondsOfDay)
+
+    var minutesOfDay =  date.getMinutes();
+    var degreesMinutes = minutesOfDay * 6 -180;
+    console.log(minutesOfDay)
+
+    var hoursOfDay =  date.getHours();
+    var degreesHours = hoursOfDay * 30 - 180;
+    console.log(hoursOfDay)
+
+    return [document.getElementById("seconds").style = 'transform: rotate(' + degreesSeconds + 'deg);',document.getElementById("minutes").style = 'transform: rotate(' + degreesMinutes + 'deg);', document.getElementById("hour").style = 'transform: rotate(' + degreesHours + 'deg);'];
+}
+
+window.onload = getSecondsSinceStartOfDay();
       
-  setInterval( function() {
-      var time = getSecondsSinceStartOfDay();
-      console.log(time);
-      // your code here
-  }, 1000);
+setInterval(getSecondsSinceStartOfDay, 1000);
+// 12 is at 180 degrees
+// 12 hours, 30 degrees per hour
+// 60 minutes, 6 degrees per minute
+// 60 seconds, 6 degrees per second
