53088bf04dff438a5a3acdfc47e4b5dab154d5a7
kpartain <karalynnleann@gmail.com>
2021-06-29 09:34:23 -0700

    Adds second solution

--- morningAlgos/w2d2.js
+++ morningAlgos/w2d2.js
@@ -1,65 +1,72 @@
 var pokemon = [
-    { "id": 1,   "name": "Bulbasaur",  "types": ["poison", "grass"] },
-    { "id": 5,   "name": "Charmeleon", "types": ["fire"] },
-    { "id": 9,   "name": "Blastoise",  "types": ["water"] },
-    { "id": 12,  "name": "Butterfree", "types": ["bug", "flying"] },
-    { "id": 16,  "name": "Pidgey",     "types": ["normal", "flying"] },
-    { "id": 23,  "name": "Ekans",      "types": ["poison"] },
-    { "id": 24,  "name": "Arbok",      "types": ["poison"] },
-    { "id": 25,  "name": "Pikachu",    "types": ["electric"] },
-    { "id": 37,  "name": "Vulpix",     "types": ["fire"] },
-    { "id": 52,  "name": "Meowth",     "types": ["normal"] },
-    { "id": 63,  "name": "Abra",       "types": ["psychic"] },
-    { "id": 67,  "name": "Machamp",    "types": ["fighting"] },
-    { "id": 72,  "name": "Tentacool",  "types": ["water", "poison"] },
-    { "id": 74,  "name": "Geodude",    "types": ["rock", "ground"] },
-    { "id": 87,  "name": "Dewgong",    "types": ["water", "ice"] },
-    { "id": 98,  "name": "Krabby",     "types": ["water"] },
-    { "id": 115, "name": "Kangaskhan", "types": ["normal"] },
-    { "id": 122, "name": "Mr. Mime",   "types": ["psychic"] },
-    { "id": 133, "name": "Eevee",      "types": ["normal"] },
-    { "id": 144, "name": "Articuno",   "types": ["ice", "flying"] },
-    { "id": 145, "name": "Zapdos",     "types": ["electric", "flying"] },
-    { "id": 146, "name": "Moltres",    "types": ["fire", "flying"] },
-    { "id": 148, "name": "Dragonair",  "types": ["dragon"] }
+    { id: 1, name: "Bulbasaur", types: ["poison", "grass"] },
+    { id: 5, name: "Charmeleon", types: ["fire"] },
+    { id: 9, name: "Blastoise", types: ["water"] },
+    { id: 12, name: "Butterfree", types: ["bug", "flying"] },
+    { id: 16, name: "Pidgey", types: ["normal", "flying"] },
+    { id: 23, name: "Ekans", types: ["poison"] },
+    { id: 24, name: "Arbok", types: ["poison"] },
+    { id: 25, name: "Pikachu", types: ["electric"] },
+    { id: 37, name: "Vulpix", types: ["fire"] },
+    { id: 52, name: "Meowth", types: ["normal"] },
+    { id: 63, name: "Abra", types: ["psychic"] },
+    { id: 67, name: "Machamp", types: ["fighting"] },
+    { id: 72, name: "Tentacool", types: ["water", "poison"] },
+    { id: 74, name: "Geodude", types: ["rock", "ground"] },
+    { id: 87, name: "Dewgong", types: ["water", "ice"] },
+    { id: 98, name: "Krabby", types: ["water"] },
+    { id: 115, name: "Kangaskhan", types: ["normal"] },
+    { id: 122, name: "Mr. Mime", types: ["psychic"] },
+    { id: 133, name: "Eevee", types: ["normal"] },
+    { id: 144, name: "Articuno", types: ["ice", "flying"] },
+    { id: 145, name: "Zapdos", types: ["electric", "flying"] },
+    { id: 146, name: "Moltres", types: ["fire", "flying"] },
+    { id: 148, name: "Dragonair", types: ["dragon"] },
 ];
 
 // Challenges - using the array of pokémon above and loops:
 // console.log the pokémon OBJECTS whose id is evenly divisible by 3
-for(var i = 0; i < pokemon.length; i++) {
-    if(pokemon[i].id % 3 == 0) {
+for (var i = 0; i < pokemon.length; i++) {
+    if (pokemon[i].id % 3 == 0) {
         console.log(pokemon[i]);
     }
 }
 
 console.log("**********************************************");
 // console.log the pokémon OBJECTS that have more than one type
-for(var i = 0; i < pokemon.length; i++) {
-    if(pokemon[i].types.length > 1) {
+for (var i = 0; i < pokemon.length; i++) {
+    if (pokemon[i].types.length > 1) {
         console.log(pokemon[i]);
     }
 }
 
 console.log("**********************************************");
 // console.log the NAMES of the pokémon whose only type is "poison"
-for(var i = 0; i < pokemon.length; i++) {
-    if(pokemon[i].types.length == 1 && pokemon[i].types[0] == "poison") {
+//first
+for (var i = 0; i < pokemon.length; i++) {
+    if (pokemon[i].types.length == 1 && pokemon[i].types[0] == "poison") {
+        console.log(pokemon[i].name);
+    }
+}
+//second
+for (var i = 0; i < pokemon.length; i++) {
+    if (pokemon[i].types == "poison") {
         console.log(pokemon[i].name);
     }
 }
 
 console.log("**********************************************");
 // console.log the FIRST TYPE of all the pokémon whose SECOND type is "flying"
-for(var i = 0; i < pokemon.length; i++) {
-    if(pokemon[i].types.length > 1 && pokemon[i].types[1] == "flying") {
+for (var i = 0; i < pokemon.length; i++) {
+    if (pokemon[i].types.length > 1 && pokemon[i].types[1] == "flying") {
         console.log(pokemon[i].types[0]);
     }
 }
 
 console.log("**********************************************");
 // Bonus Challenge: console.log the REVERSE of the NAMES of the pokémon whose ONLY type is "poison"
-for(var i = 0; i < pokemon.length; i++) {
-    if(pokemon[i].types.length == 1 && pokemon[i].types[0] == "poison") {
+for (var i = 0; i < pokemon.length; i++) {
+    if (pokemon[i].types.length == 1 && pokemon[i].types[0] == "poison") {
         console.log(pokemon[i].name.split("").reverse().join(""));
     }
 }
