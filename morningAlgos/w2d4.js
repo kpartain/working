2e22fa4beca39f3bb1251d47227ff786ac95ae46
kpartain <karalynnleann@gmail.com>
2021-07-01 09:14:28 -0700

    Completes w2d4 algo

--- morningAlgos/w2d4.js
+++ morningAlgos/w2d4.js
@@ -6,13 +6,21 @@ var arr2d = [ [2, 5, 8],
               [5, 7, 7] ];
     
 // We can console.log the `8` in this array if we
-console.log(arr2d[0][2]);
+// console.log(arr2d[0][2]);
 // the first index `0` will select the `[2, 5, 8]` sub-array
 // the second index `2` will select the `8` out of that sub-array
 
 // Could we determine if a certain value was present? Write a function called isPresent2d(arr2d, value) that returns true if the value is present and false otherwise
 // Note - Don't assume the array will always be the same size, we must rely on its .length property
-// Hint - Can we put a for loop inside another for loop?
+// Hint - Can we put a for loop inside another for loop? 
+function isPresent2d(arr2d, value) {
+    for(var i=0; i< arr2d.length; i++) {
+        return (arr2d[i].includes(value));
+    }
+}
+
+var myTest = isPresent2d(arr2d, 8);
+console.log(myTest);
 
 // Flatten Array
 // Given a 2 dimensional array (an array containing arrays), return a new array containing just the values from 
